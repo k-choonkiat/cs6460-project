d58723ab636118086e8655a2e922104e7825533e
ChoonKiat <ckang86@gatech.edu>
2025-04-20 23:59:46 +0800

    use host 0.0.0.0

--- run_waitress.py
+++ run_waitress.py
@@ -5,5 +5,4 @@ from myapp import app
 from waitress import serve
 
 if __name__ == "__main__":
-    print("Serving on http://127.0.0.1:8080")
-    serve(app, host="127.0.0.1", port=8080)  # Serving the app using Waitress
+    serve(app, host="0.0.0.0", port=8080)  # Serving the app using Waitress
